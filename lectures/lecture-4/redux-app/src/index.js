200616c63b311e36169679063afb15c0fba35341
Yuliana Romaniv <Yuliana.Romaniv@eleks.com>
2021-10-13 15:31:00 +0300

    setup basic redux app

---
+++ lectures/lecture-4/redux-app/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+// import { RootProvider } from "./components/root-provider/root-provider";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import "semantic-ui-css/semantic.min.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
